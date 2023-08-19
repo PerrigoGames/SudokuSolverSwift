68519ee4a6f0b3749757dbc09e4630352dd6be00
Corey Perrigo <corey@perrigogames.com>
2023-08-19 10:26:46 -0700

    Initial Commit

---
+++ SudokuSolver/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SudokuSolver
+//
+//  Created by Corey Perrigo on 8/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SudokuSolver/SudokuSolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuSolverApp.swift
+//  SudokuSolver
+//
+//  Created by Corey Perrigo on 8/19/23.
+//
+
+import SwiftUI
+
+@main
+struct SudokuSolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
